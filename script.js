a7af9ff87ef8864dac8f43424cb4d6226fed37f5
justinmo128 <j.mo1@share.epsb.ca>
2022-09-13 11:22:19 -0600

    make quiz questions and finish about page, make nav red when on current page

--- script.js
+++ script.js
@@ -8,8 +8,32 @@ function play() {
     audio.play();
 }
 
+//plays sound when on current page
+function navbad() {
+    var audio = document.getElementById("select");
+    audio.play();
+}
+
 //shows navigation on index
 function showNav() {
     document.getElementById('nav').style.display = "block";
     document.getElementById('hideme').style.display = "none";
+}
+
+//read more button
+function readMore() {
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var button = document.getElementById("readMore");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        button.innerHTML = "Read more";
+        more.style.display = "none";
+    } 
+    else {
+        dots.style.display = "none";
+        button.innerHTML = "Read less";
+        more.style.display = "inline";
+    }
 }
