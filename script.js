4c5797a24f3fa4814c6dfe7b09c7add47e1ef183
justinmo128 <j.mo1@share.epsb.ca>
2022-09-14 11:21:42 -0600

    tabs in character section, more divs, cleanup

--- script.js
+++ script.js
@@ -20,20 +20,24 @@ function showNav() {
     document.getElementById('hideme').style.display = "none";
 }
 
-//read more button
-function readMore() {
-    var dots = document.getElementById("dots");
-    var more = document.getElementById("more");
-    var button = document.getElementById("readMore");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        button.innerHTML = "Read more";
-        more.style.display = "none";
-    } 
-    else {
-        dots.style.display = "none";
-        button.innerHTML = "Read less";
-        more.style.display = "inline";
+//tabs for character section in about page
+function char(evt, charName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-}
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(charName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
