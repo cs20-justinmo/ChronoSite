41479d91f08301d61bd06bc45b8a417c2d91e3eb
justinmo128 <j.mo1@share.epsb.ca>
2022-09-21 11:20:25 -0600

    quiz formatting

--- quiz.js
+++ quiz.js
@@ -5,22 +5,25 @@ function checkAnswers() {
     //Input
     let ans1 = document.getElementById('q1').value;
     let ans2 = document.getElementById('q2').value;
-    ans2 = ans2.toLowerCase();
     let ans3 = document.getElementById('q3').value;
-    ans3 = ans3.toLowerCase();
     let ans4 = document.getElementById('q4').value;
-    ans4 = ans4.toLowerCase();
     let ans5 = document.getElementById('q5').value;
-    ans5 = ans5.toLowerCase();
     let ans6 = document.getElementById('q6').value;
-    ans6 = ans6.toLowerCase();
     let ans7 = document.getElementById('q7').value;
-    ans7 = ans7.toLowerCase();
     let ans8 = document.getElementById('q8').value;
-    ans8 = ans8.toLowerCase();
     quizScore = 0;
 
     //Process
+    //Convert to lowercase
+    ans2 = ans2.toLowerCase();
+    ans3 = ans3.toLowerCase();
+    ans4 = ans4.toLowerCase();
+    ans5 = ans5.toLowerCase();
+    ans6 = ans6.toLowerCase();
+    ans7 = ans7.toLowerCase();
+    ans8 = ans8.toLowerCase();
+
+    //Check answers
     if (ans1 == '1995') {
         quizScore++;
     }
@@ -59,10 +62,13 @@ function checkAnswers() {
             quizScore++;
         }
     if (ans8 == 'technique' ||
-        ans8 == 'tech') {
+        ans8 == 'tech' ||
+        ans8 == 'techniques' ||
+        ans8 == 'techs') {
             quizScore++;
         }
 
+    //Calculate percentage
     let percentScore = quizScore / 8 * 100;
 
     //Output
